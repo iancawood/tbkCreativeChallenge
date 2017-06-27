5a5e2cb138b0845dbe7fe520354a1d47a816439d
Ian Wood <ian.wood108@gmail.com>
2017-06-26 22:54:10 -0400

    Added fade in and out

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
 $(document).ready(function() {
-	
+	setTimeout(function() {
+		$(".pop-up").fadeIn("slow");
+ 	}, 3000);
+
+ 	$(".pop-up .close").on("click", function() {
+ 		$(".pop-up").fadeOut("slow");
+ 	});
+
+ 	$('.pop-up .form').submit(function(e){
+    	e.preventDefault();
+
+    	// AJAX
+
+    	$(".pop-up").fadeOut("slow");
+    });
 });
